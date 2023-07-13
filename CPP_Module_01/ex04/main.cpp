7395fad0af4faa736dacc68c9343433b4286ca09
Andreia Filipa Araujo Campos <anaraujo@c1r7s2.42porto.com>
2023-07-13 23:19:27 +0100

    cpp01 completed

--- CPP_Module_01/ex04/main.cpp
+++ CPP_Module_01/ex04/main.cpp
@@ -1,17 +1,64 @@
-void ceate_new_file()
+#include "sed.h"
+
+/*The c_str() function in C++ converts a string to an array of characters
+and terminates this array with a null character at the end.*/
+/*str.find retorna o valor do indice onde encontra a string.
+Se o valor do indice corresponder ao valor do indice onde eu estou, eu vou ja substituir se nao vamos avancar*/
+int	replace(std::string str, char **argv)
 {
-	
+	std::ofstream	outfile;
+	int pos;
+
+	outfile.open((std::string(argv[1]) + ".replace").c_str());
+	if(!outfile.is_open())
+		return (1);
+	for(int i = 0; i < (int)str.size(); i++)
+	{
+		pos = str.find(argv[2], i);
+		if (pos != -1 && pos == i)
+		{
+			outfile << argv[3];
+			i += std::string(argv[2]).size() - 1;
+		}
+		else
+			outfile << str[i];
+	}
+	outfile.close();
+	return (0);
+}
+
+void read_file(char *str, char **argv)
+{
+	std::ifstream infile;
+	std:: string str1;
+
+	infile.open(str);
+	if(!infile.is_open())
+	{
+		std :: cout << "No such file or directory" <<std::endl;
+		return ;
+	}
+	std::stringstream buffer;
+	buffer << infile.rdbuf();
+	std::string file_contents;
+	file_contents = buffer.str();
+	infile.close();
+	replace(file_contents, argv);
 }
 
 int	main(int argc, char **argv)
 {
+
 	if (argc == 4)
 	{
-
+		read_file(argv[1], argv);
 	}
 	else
 	{
 		std::cout <<"Wrong inputs." <<std::endl;
-		return (0);
+		return (1);
 	}
+	return (0);
 }
+
+/*https://www.youtube.com/watch?v=q97E8rOFWSU*/
---
+++ CPP_Module_01/ex04/sed.h
@@ -0,0 +1,9 @@
+#ifndef SED_HH
+#define SED_HH
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
+#endif
