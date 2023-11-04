90199f88930e15b9f8e973c6c2c4e88850c16439
Andreia Campos <andreiacampos98.15@gmail.com>
2023-11-04 11:01:27 +0000

    Update MutantStack.hpp

--- CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -2,9 +2,12 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <array>
+#include <iostream>
+
 class MutantStack{
   private:
-
+    std::array  _arr;
   public:
     MutantStack();
     ~MutantStack();
