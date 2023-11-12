1b4b13d9e9357a86d80db2549478a3d59bc2e045
Andreia Filipa Araujo Campos <anaraujo@c1r7s2.42porto.com>
2023-11-12 15:08:00 +0000

    cpp08 doneeee

--- CPP_Module_07/ex00/main.cpp
+++ CPP_Module_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anaraujo <anaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 18:54:00 by anaraujo          #+#    #+#             */
-/*   Updated: 2023/11/01 11:07:29 by anaraujo         ###   ########.fr       */
+/*   Updated: 2023/11/11 17:52:47 by anaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ int	main( void )
 	int a = 2;
 	int b = 3;
 
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 	::swap( a, b );
+	std::cout << "Swap\n";
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl; // implicit instantiation
 	std::cout << "max( a, b ) = " << ::max<int>( a, b ) << std::endl; // explicit instantiation
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
+	std::cout << "Swap\n";
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
